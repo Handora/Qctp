cdef7cde8a60dbd8538782310d53e99bf137e899
Handora <qcdsr970209@gmail.com>
2018-03-11 17:47:30 +0800

    clean

--- qio.h
+++ qio.h
@@ -19,37 +19,16 @@ struct rio_t {
     char rio_buf[RIO_BUFSIZE];
 };
 
-ssize_t
-readn(int fd, void *vptr, size_t n);
-
-ssize_t
-writen(int fd, SSL *CTX, void *vptr, size_t n);
-
-int
-close_socket(int sock);
-
-handler_t *
-Signal(int signum, handler_t *handler);
-
-void
-rio_readinitb(struct rio_t *rp, int fd);
-
-ssize_t
-rio_read(struct rio_t *rp, SSL *CTX, char *usrbuf, size_t n);
-
-ssize_t
-rio_readlineb(struct rio_t *rp, SSL *CTX, void *buf, size_t maxline);
-
-ssize_t
-rio_readn(struct rio_t *rp, SSL* CTX, void *buf, size_t maxn);
-
-char *
-Sock_ntop(const struct sockaddr *sa, socklen_t salen);
-
-char *
-sock_ntop(const struct sockaddr *sa, socklen_t salen);
-
-int
-tcp_listen(const char *host, const char *serv, socklen_t *len);
+ssize_t readn(int fd, void *vptr, size_t n);
+ssize_t writen(int fd, SSL *CTX, void *vptr, size_t n);
+int close_socket(int sock);
+handler_t *Signal(int signum, handler_t *handler);
+void rio_readinitb(struct rio_t *rp, int fd);
+ssize_t rio_read(struct rio_t *rp, SSL *CTX, char *usrbuf, size_t n);
+ssize_t rio_readlineb(struct rio_t *rp, SSL *CTX, void *buf, size_t maxline);
+ssize_t rio_readn(struct rio_t *rp, SSL* CTX, void *buf, size_t maxn);
+char *Sock_ntop(const struct sockaddr *sa, socklen_t salen);
+char *sock_ntop(const struct sockaddr *sa, socklen_t salen);
+int tcp_listen(const char *host, const char *serv, socklen_t *len);
 
 #endif
